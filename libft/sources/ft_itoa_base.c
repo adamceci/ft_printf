5f18060d0123c90b81e36be922bfbcd70dfbb184
Adam Ceciora <aceciora@w-r4-p2.s19.be>
2019-06-15 10:15:22 +0200

    itoa_base with 42 norm

--- libft/sources/ft_itoa_base.c
+++ libft/sources/ft_itoa_base.c
@@ -1,4 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aceciora <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/15 09:54:14 by aceciora          #+#    #+#             */
+/*   Updated: 2019/06/15 09:54:17 by aceciora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
+
+void	change_base(int value, int base, char *str, int i)
+{
+	int	rest;
+
+	while (value > 0)
+	{
+		rest = value % base;
+		if (rest >= 0 && rest <= 9)
+			str[i - 1] = rest + '0';
+		else
+			str[i - 1] = rest - 10 + 'A';
+		value /= base;
+		i--;
+	}
+}
 
 char	*ft_itoa_base(int value, int base)
 {
@@ -6,7 +34,6 @@ char	*ft_itoa_base(int value, int base)
 	int		i;
 	int		sign;
 	int		num;
-	int		rest;
 
 	sign = 0;
 	i = 0;
@@ -24,16 +51,7 @@ char	*ft_itoa_base(int value, int base)
 	}
 	str = (char*)malloc(sizeof(*str) * i + 1);
 	str[i] = '\0';
-	while (value > 0)
-	{
-		rest = value % base;
-		if (rest >= 0 && rest <= 9)
-			str[i - 1] = rest + '0';
-		else
-			str[i - 1] = rest - 10 + 'A';
-		value /= base;
-		i--;
-	}
+	change_base(value, base, str, i);
 	if (sign)
 		str[0] = '-';
 	return (str);
